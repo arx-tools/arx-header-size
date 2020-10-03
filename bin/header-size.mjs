d333939c5dfceca3d3bd73bbf787230df218a46b
Lajos.Meszaros <m_lajos@hotmail.com>
2020-10-03 20:46:20 +0200

    Added checks and validation

--- bin/header-size.mjs
+++ bin/header-size.mjs
@@ -4,15 +4,41 @@ import fs from 'fs'
 import minimist from 'minimist'
 import { toHex } from '../src/helpers.mjs'
 import { DLF_HEADER_SIZE, FTS_HEADER_SIZE, FTS_UNIQUE_HEADER_SIZE } from '../src/constants.mjs'
-import { exit } from 'process'
+import { fileExists, getPackageVersion } from './helpers.mjs'
 
 const args = minimist(process.argv.slice(2), {
-  boolean: ['hex', 'verbose']
+  boolean: ['hex', 'verbose', 'version']
 })
+
+if (args.version) {
+  console.log(getPackageVersion())
+  process.exit(0)
+}
 
 const filename = args._[0]
 
+let hasErrors = false
+
+if (!filename) {
+  console.error('error: filename not specified')
+  hasErrors = true
+} else if (!fileExists(filename)) {
+  console.error('error: given file does not exist')
+  hasErrors = true
+}
+
+if (hasErrors) {
+  process.exit(1)
+}
+
 const extension = filename.match(/\.([a-zA-Z]+)$/)[1].toLowerCase()
+const supportedExtensions = ['dlf', 'fts', 'llf']
+
+if (!supportedExtensions.includes(extension)) {
+  console.error('error: unsupported file format')
+  process.exit(1)
+}
+
 const outputRequestedAsHex = args.hex
 
   ; (async () => {
