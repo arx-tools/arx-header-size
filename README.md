917ec80e9a5d5788f6b9e532b741fa596e580f02
Lajos Meszaros <m_lajos@hotmail.com>
2024-12-20 23:34:19 +0100

    feat: add support for uncompressed FTS files #1

--- README.md
+++ README.md
@@ -17,7 +17,29 @@ as an offset for [node-pkware](https://github.com/arx-tools/node-pkware)
 
 ( https://wiki.arx-libertatis.org/Filetypes )
 
-## Usage
+## JS API
+
+```ts
+const rawFtsData: ArrayBuffer = ... // load the contents of an FTS file as an ArrayBuffer
+
+const { total, header, uniqueHeaderSize, numberOfUniqueHeaders, compression } = getHeaderSize(rawFtsData)
+switch (compression) {
+  case 'none': {
+    // file is uncompressed
+    break
+  }
+  case 'partial': {
+    // first <total> bytes are uncompressed, the rest is compressed
+    break
+  }
+  case 'full': {
+    // the whole file is compressed
+    break
+  }
+}
+```
+
+## CLI
 
 `npx arx-header-size "C:\arx\arx-pak-full\final\GAME\GRAPH\Levels\level8\fast.fts"`
 
@@ -62,3 +84,15 @@ header size: 280 (0x118)
 unique header size: 768 (0x300)
 number of unique headers: 2
 ```
+
+### Uncompressed FTS files in Arx Libertatis 1.3
+
+Arx Libertatis feature: setting the uncompressed bytes' size in the header of an FTS file to 0 gets
+interpreted as the file being uncompressed
+
+source of discussion: https://arx-libertatis.org/irclogs/2022/%23arx.2022-09-06.log
+and https://arx-libertatis.org/irclogs/2022/%23arx.2022-09-07.log
+
+implemented in: https://github.com/arx/ArxLibertatis/commit/2d2226929780b6202f54982bacc79ddf75dbec53
+
+available in Arx Libertatis 1.3 snapshots that came after `2022-09-17` in https://arx-libertatis.org/files/snapshots/
--- src/index.js
+++ src/index.js
@@ -27,12 +27,28 @@ export function getHeaderSize(buffer, format) {
     case 'fts':
       {
         const dataView = new DataView(buffer);
-        const numberOfUniqueHeaders = dataView.getInt32(256, true);
+        const uncompressedSize = dataView.getInt32(264, true);
+        /**
+         * Arx Libertatis feature: setting the uncompressed bytes' size in the header of an FTS file to 0 gets
+         * interpreted as the file being uncompressed
+         *
+         * source of discussion: https://arx-libertatis.org/irclogs/2022/%23arx.2022-09-06.log
+         * and https://arx-libertatis.org/irclogs/2022/%23arx.2022-09-07.log
+         *
+         * implemented in: https://github.com/arx/ArxLibertatis/commit/2d2226929780b6202f54982bacc79ddf75dbec53
+         *
+         * available in Arx Libertatis 1.3 snapshots that came after `2022-09-17` in https://arx-libertatis.org/files/snapshots/
+         */
+        if (uncompressedSize === 0) {
+          sizes.compression = 'none';
+        } else {
+          const numberOfUniqueHeaders = dataView.getInt32(256, true);
 
-        sizes.total = FTS_HEADER_SIZE + FTS_UNIQUE_HEADER_SIZE * numberOfUniqueHeaders;
-        sizes.header = FTS_HEADER_SIZE;
-        sizes.uniqueHeaderSize = FTS_UNIQUE_HEADER_SIZE;
-        sizes.numberOfUniqueHeaders = numberOfUniqueHeaders;
+          sizes.total = FTS_HEADER_SIZE + FTS_UNIQUE_HEADER_SIZE * numberOfUniqueHeaders;
+          sizes.header = FTS_HEADER_SIZE;
+          sizes.uniqueHeaderSize = FTS_UNIQUE_HEADER_SIZE;
+          sizes.numberOfUniqueHeaders = numberOfUniqueHeaders;
+        }
       }
       break;
     case 'llf':
